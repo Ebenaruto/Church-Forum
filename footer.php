ab2fe14260348013ee19f0761a04cca17dd230dc
Ebenaruto <omartial30@gmail.com>
2020-05-01 18:18:23 +0000

    header and footer

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer class="page-footer text-center font-small wow fadeIn" id="mafoot">
+
+    
+    <!--Copyright-->
+    <div class="footer-copyright py-3">
+      © 2020 Copyright:
+      <a href="#"> AD_Cissin_1 </a>
+    </div>
+    <!--/.Copyright-->
+
+</footer>
+  <!--/.Footer-->
---
+++ header.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Material Design for Bootstrap</title>
+  <!-- MDB icon -->
+  <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="fontawesome/css/all.css">
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="google_font">
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="MDB/css/bootstrap.min.css">
+  <!-- Material Design Bootstrap -->
+  <link rel="stylesheet" href="MDB/css/mdb.min.css">
+  <!-- Your custom styles (optional) -->
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  
+   <!-- Navbar -->
+   <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
+    <div class="container">
+      <!-- Collapse -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Links -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <a class="navbar-brand" href="#"><img src="img/logoAD.png" alt="logo" id="log"></a>
+        <!-- Left -->
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#" id="men">Acceuil
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" id="men">Actualité</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" id="men">Enseignements</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" id="men">Structure de l'Eglise</a>
+          </li>
+        </ul>
+
+        <!-- Right -->
+        <ul class="navbar-nav nav-flex-icons">
+          <li class="nav-item">
+            <a href="https://www.facebook.com/" class="nav-link" target="_blank">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="https://twitter.com/" class="nav-link" target="_blank">
+              <i class="fab fa-twitter"></i>
+            </a>
+          </li>
+          
+        </ul>
+      
+      </div>
+
+    </div>
+  </nav>
+  <!-- Navbar -->
+  
+  
+  <div class="view">
+    <!-- Mask & flexbox options-->
+    <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+
+      <!-- Content -->
+      <div class="text-center white-text mx-5 wow fadeIn">
+      <h2 class="animated fadeIn"><span class="text"> Eglise des Assemblées de Dieu de Cissin1</span></h2>
+      <h2><span class="thm"> <blockquote> "Seigneur, que veux tu que je fasse pour toi?"</blockquote> </span></h2>
+      </div>
+    <!-- Mask & flexbox options-->
+    </div>
+  </div>
+  <!-- Full Page Intro -->
+  <?php include_once('footer.php');?>
+
+
+  <!-- jQuery -->
+  <script type="text/javascript" src="MDB/js/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="MDB/js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="MDB/js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="MDB/js/mdb.min.js"></script>
+  <!-- Your custom scripts (optional) -->
+  <script type="text/javascript" src="script.js"></script>
+  <script type="text/javascript">
+    // Animations initialization
+    new WOW().init();
+
+  </script>
+
+
+</body>
+</html>

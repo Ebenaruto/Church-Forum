23b57a727d63ef06700bee4ef1bbc106103a6d55
armel <armelzong@gmail.com>
2020-05-02 06:52:53 +0000

    Transforming

---
+++ connexion.php
@@ -0,0 +1,16 @@
+<div class="row">
+    
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        <form action="" method="post">
+            <input type="text" name="username" id="username" placeholder="Username or Email ">
+            <input type="password" name="password" id="password" class="form-control" required="required" placeholder="Password">
+            <input type="submit">
+        </form>
+    </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        
+    </div>
+</div>
--- header.php
+++ header.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Material Design for Bootstrap</title>
-  <!-- MDB icon -->
-  <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="fontawesome/css/all.css">
-  <!-- Google Fonts Roboto -->
-  <link rel="stylesheet" href="google_font">
-  <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" href="MDB/css/bootstrap.min.css">
-  <!-- Material Design Bootstrap -->
-  <link rel="stylesheet" href="MDB/css/mdb.min.css">
-  <!-- Your custom styles (optional) -->
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-  
-   <!-- Navbar -->
+     <!-- Navbar -->
    <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
     <div class="container">
       <!-- Collapse -->
@@ -84,26 +62,3 @@
     <!-- Mask & flexbox options-->
     </div>
   </div>
-  <!-- Full Page Intro -->
-  <?php include_once('footer.php');?>
-
-
-  <!-- jQuery -->
-  <script type="text/javascript" src="MDB/js/jquery.min.js"></script>
-  <!-- Bootstrap tooltips -->
-  <script type="text/javascript" src="MDB/js/popper.min.js"></script>
-  <!-- Bootstrap core JavaScript -->
-  <script type="text/javascript" src="MDB/js/bootstrap.min.js"></script>
-  <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="MDB/js/mdb.min.js"></script>
-  <!-- Your custom scripts (optional) -->
-  <script type="text/javascript" src="script.js"></script>
-  <script type="text/javascript">
-    // Animations initialization
-    new WOW().init();
-
-  </script>
-
-
-</body>
-</html>

c3139f012bfeedd4ebec30d83c4de655b81ae672
armel <armelzong@gmail.com>
2020-05-03 21:29:31 +0000

    tired

--- header.php
+++ header.php
@@ -39,7 +39,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#" id="men">Actualité</a>
+            <a class="nav-link" href="article.php" id="men">Actualité</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#" id="men">Enseignements</a>

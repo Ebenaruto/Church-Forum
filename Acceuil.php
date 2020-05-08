3153a164a883d45e55cc3ee322f7ca835093e16c
armel <armelzong@gmail.com>
2020-05-08 12:34:29 +0000

    inspiration_again

--- Acceuil.php
+++ Acceuil.php
@@ -1,14 +1,18 @@
 <div class="row intern ">
-      <div class=" col-lg-4 col-md-4 col-sm-12 col-xm-12">
+      <div class=" col-lg-3 col-md-3 col-sm-12 col-xm-12">
 
       </div>
-      <div class="bg-transparent  acceuil col-lg-4 col-md-4 col-sm-12 col-xm-12">
-            <h2 class=""><span class="text"> Eglise des Assemblées de Dieu de Cissin1</span></h2>
+      <div class="bg-transparent  acceuil col-lg-6 col-md-6 col-sm-12 col-xm-12">
+            <h2 class=""><span class="text"> Eglise des <strong>Assemblées de Dieu de Cissin1 </strong></span></h2>
+            <br>
+            <hr class="hr">
+            <br>
             <h2><span class="thm">
-                        <blockquote> "Seigneur, que veux tu que je fasse pour toi?"</blockquote>
-                  </span></h2>
+                        <blockquote> <strong>Seigneur, que veux tu que je fasse ? </strong></blockquote>
+                  </span>
+            </h2>
       </div>
-      <div class=" col-lg-4 col-md-4 col-sm-12 col-xm-12">
+      <div class=" col-lg-3 col-md-3 col-sm-12 col-xm-12">
 
       </div>
 </div>

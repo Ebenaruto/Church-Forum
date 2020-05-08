8f55804bd26d954f5ad604277e48b528216c5fb5
armel <armelzong@gmail.com>
2020-05-08 12:11:26 +0000

    inspiration_again

--- header.php
+++ header.php
@@ -19,14 +19,14 @@
 
 <body>
   <!-- Navbar -->
-
- <nav class="nav bg-transparent justify-content-left navi">
-   <a class="nav-link " href="index.php">Acceuil</a>
-   <a class="nav-link " href="articles">Articles</a>
-   <a class="nav-link " href="connexion.php">Connexion/Inscription</a>
-   <a class="nav-link "><i class="fab fa-facebook"></i></a>
-   <a class="nav-link "><i class="fab fa-twitter"></i></a>
- </nav>
+  <nav class="nav bg-transparent justify-content-left navi active ">
+    <img src="img\logoAD.png" alt="logo" class="logo">
+    <a class="nav-link " href="index.php">Acceuil</a>
+    <a class="nav-link " href="article.php">Articles</a>
+    <a class="nav-link " href="connexion.php">Connexion\Inscription</a>
+    <a class="nav-link  " href="https://www.facebook.com"><i class="fab fa-facebook"></i></a>
+    <a class="nav-link "  href="https://www.twitter.com"><i class="fab fa-twitter"></i></a>
+  </nav>
   <!-- Tab panes -->
   <div class="tab-content">
     <div class="tab-pane fade show active" id="tab1Id" role="tabpanel"></div>
@@ -36,4 +36,3 @@
     <div class="tab-pane fade" id="tab5Id" role="tabpanel"></div>
   </div>
   <!-- Navbar -->
-  
